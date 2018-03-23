0d2754a9ab3d37a732514a6f3594535aa3c2ef34
Jimmy Chao <daizenga@gmail.com>
2018-03-23 15:54:04 -0400

    Add step 3

--- bs/env.rb
+++ bs/env.rb
@@ -1,8 +1,47 @@
-REPL_ENV = {
-  :+ => -> (a, b) { a + b },
+DEFAULT_ENV = {
+  :+ => -> (*args) { args.reduce(&:+) },
   :- => -> (a, b) { a - b },
   :* => -> (a, b) { a * b },
   :/ => -> (a, b) { a / b },
   :% => -> (a, b) { a % b },
   :** => -> (a, b) { a ** b },
 }
+
+
+class Env
+  attr_accessor :data, :outer
+  def initialize(outer = nil)
+    @data = {}
+    @outer = outer
+  end
+
+  def find(key)
+    if data.key?(key)
+      data[key]
+    elsif outer
+      outer.find(key)
+    else
+      nil
+    end
+  end
+
+  def set(key, val)
+    data[key] = val
+  end
+
+  def key?(key)
+    data.key?(key)
+  end
+
+  def missing?(key)
+    !find(key)
+  end
+
+  def get(key)
+    if val = data.find(key)
+      val
+    else
+      raise "exception: key #{key} does not found"
+    end
+  end
+end
